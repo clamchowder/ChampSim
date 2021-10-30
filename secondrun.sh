c5000a3e37ce52a4cddd3309193f5985aeff9bd3
clamchowder <lamchester@gmail.com>
2021-10-30 05:42:31 +0000

    prepare for run

---
+++ secondrun.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#find srv_traces -name 'srv*' | xargs -n 1 -P 32 ./singlerun.sh 8m_l3
+#find srv_traces -name 'srv*' | xargs -n 1 -P 32 ./singlerun.sh 16m_l3
+#find srv_traces -name 'srv*' | xargs -n 1 -P 32 ./singlerun.sh 32m_l3
+#find srv_traces -name 'srv*' | xargs -n 1 -P 32 ./singlerun.sh 96m_l3
+
+find dpc3_traces -name '*' | xargs -n 1 -P 32 ./singlerun.sh zen3_16kpage
+find dpc3_traces -name '*' | xargs -n 1 -P 32 ./singlerun.sh zen3_3200mhz
+find dpc3_traces -name '*' | xargs -n 1 -P 32 ./singlerun.sh m1
+find dpc3_traces -name '*' | xargs -n 1 -P 32 ./singlerun.sh m1core_zen3cache
