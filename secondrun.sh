571c9e615151de93f8a1575e9e2a1089bca0fed5
clamchowder <lamchester@gmail.com>
2021-12-06 09:23:58 +0000

    get permission bit on

--- secondrun.sh
+++ secondrun.sh
@@ -4,7 +4,12 @@
 #find srv_traces -name 'srv*' | xargs -n 1 -P 32 ./singlerun.sh 32m_l3
 #find srv_traces -name 'srv*' | xargs -n 1 -P 32 ./singlerun.sh 96m_l3
 
-find dpc3_traces -name '*' | xargs -n 1 -P 32 ./singlerun.sh zen3_16kpage
-find dpc3_traces -name '*' | xargs -n 1 -P 32 ./singlerun.sh zen3_3200mhz
-find dpc3_traces -name '*' | xargs -n 1 -P 32 ./singlerun.sh m1
-find dpc3_traces -name '*' | xargs -n 1 -P 32 ./singlerun.sh m1core_zen3cache
+#find dpc3_traces -name '*' | xargs -n 1 -P 32 ./singlerun.sh zen3_16kpage
+#find dpc3_traces -name '*' | xargs -n 1 -P 32 ./singlerun.sh zen3_3200mhz
+#find dpc3_traces -name '*' | xargs -n 1 -P 32 ./singlerun.sh m1
+#find dpc3_traces -name '*' | xargs -n 1 -P 32 ./singlerun.sh m1core_zen3cache
+
+find dpc3_traces -name '*' | xargs -n 1 -P 32 ./singlerun.sh glc 
+find clamtraces -name '*' | xargs -n 1 -P 32 ./singlerun.sh glc 
+find dpc3_traces -name '*' | xargs -n 1 -P 32 ./singlerun.sh glc_32k
+find clamtraces -name '*' | xargs -n 1 -P 32 ./singlerun.sh glc_32k 
